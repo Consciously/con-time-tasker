c012a064d0b667b22b7840da1481796dab3335d3
Stefan Ihle <stefanihle@gmx.net>
2023-08-08 11:41:24 +0200

    add sanity and custom google fonts

---
+++ src/app/fonts.ts
@@ -0,0 +1,13 @@
+import { Oxanium, Nunito } from 'next/font/google';
+
+export const oxanium = Oxanium({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['200', '400', '600'],
+});
+
+export const nunito = Nunito({
+	subsets: ['latin'],
+	display: 'swap',
+	weight: ['200', '400', '600'],
+});

38ac9633c5fc8821fc8319649ff2af995f3ad31f
Stefan Ihle <stefanihle@gmx.net>
2023-08-09 14:39:30 +0200

    modify user info box in header

--- src/app/_components/ui/header/Header.tsx
+++ src/app/_components/ui/header/Header.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
-import { nunito, oxanium } from '@/app/(site)/fonts';
 import Logo from './Logo';
+import { oxanium } from '@/app/(site)/fonts';
+import UserInfo from './UserInfo';
 
 const Header = () => {
 	return (
-		<header
-			className={`${nunito.className} flex bg-stone-900 p-4 justify-between items-center h-[20vh]`}
-		>
+		<header className='flex bg-stone-900 p-4 justify-between items-center h-[20vh]'>
 			<Logo />
 			<div>
 				<h1
@@ -15,25 +14,7 @@ const Header = () => {
 					Time Tasker
 				</h1>
 			</div>
-			<div className='flex flex-col justify-items-center items-center bg-orange-gradient-right rounded-md p-1 max-w-sm'>
-				<div className='bg-stone-900 p-4'>
-					<h3
-						className={`${oxanium.className} text-4xl text-stone-300 text-center font-semibold mb-2`}
-					>
-						Hey Dude!
-					</h3>
-					<p
-						className={`${oxanium.className} text-base text-stone-300 font-semibold text-center mt-2`}
-					>
-						You have to
-					</p>
-					<p
-						className={`${oxanium.className} text-base text-stone-300 font-semibold text-center mt-2`}
-					>
-						sign in or sign up!
-					</p>
-				</div>
-			</div>
+			<UserInfo />
 		</header>
 	);
 };

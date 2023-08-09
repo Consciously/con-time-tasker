6c603dea052698aa9cea9f0e687860c53bfe513c
Stefan Ihle <stefanihle@gmx.net>
2023-08-09 13:43:59 +0200

    style sign out button

--- src/app/_components/ui/sidebar/MenuIcon.tsx
+++ src/app/_components/ui/sidebar/MenuIcon.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 
 const MenuIcon = () => {
 	return (
-		<div className='w-14 h-14 bg-orange-600 border-2 border-stone-900 rounded-md mx-auto shadow-lg'>
+		<div className='w-14 h-14 border-2 border-orange-600 rounded-md mx-auto shadow-lg'>
 			<div className='flex flex-col justify-center items-center h-full w-full mt-1'>
-				<div className='w-8 border-2 border-stone-900 mb-2'></div>
-				<div className='w-8 border-2 border-stone-900 mb-2'></div>
-				<div className='w-8 border-2 border-stone-900 mb-2'></div>
+				<div className='w-8 border-2 border-orange-600 mb-2'></div>
+				<div className='w-8 border-2 border-orange-600 mb-2'></div>
+				<div className='w-8 border-2 border-orange-600 mb-2'></div>
 			</div>
 		</div>
 	);
---
+++ src/app/_components/ui/sidebar/SignOut.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { SignOutButton, currentUser } from '@clerk/nextjs';
+import { FaSignOutAlt } from 'react-icons/fa';
+
+const SignOut = async () => {
+	const user = await currentUser();
+
+	return (
+		<div className='w-14 h-14 mx-auto text-center mt-8'>
+			{user?.id && (
+				<SignOutButton>
+					<button className='p-4 text-center text-xl rounded-full shadow-lg text-orange-600 border-2 border-orange-600'>
+						<FaSignOutAlt />
+					</button>
+				</SignOutButton>
+			)}
+		</div>
+	);
+};
+
+export default SignOut;

cb65d854a0897b3452f02bcece411cbd68069b5c
Stefan Ihle <stefanihle@gmx.net>
2023-08-15 17:19:12 +0200

    add open menu state to menuicon

--- src/app/_components/ui/sidebar/MenuIcon.tsx
+++ src/app/_components/ui/sidebar/MenuIcon.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+'use client';
+
+import React, { useState } from 'react';
 
 const MenuIcon = () => {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const handleMenuIcon = () => {
+		setIsOpen(prevState => !prevState);
+	};
+
 	return (
-		<div className='w-14 h-14 border-2 border-orange-600 rounded-md mx-auto shadow-lg'>
-			<div className='flex flex-col justify-center items-center h-full w-full mt-1'>
-				<div className='w-8 border-2 border-orange-600 mb-2'></div>
-				<div className='w-8 border-2 border-orange-600 mb-2'></div>
-				<div className='w-8 border-2 border-orange-600 mb-2'></div>
+		<div
+			className='w-14 h-14 border-2 border-orange-600 rounded-md mx-auto shadow-lg'
+			onClick={handleMenuIcon}
+		>
+			<div className='flex flex-col justify-center items-center h-full w-full'>
+				<div
+					className={`w-8 h-1 bg-orange-600 transform transition-transform duration-300 ease-in-out ${
+						isOpen ? 'origin-center translate-y-1 rotate-45' : 'mb-1'
+					}`}
+				></div>
+				<div
+					className={`w-8 h-1 bg-orange-600 transform transition-transform duration-300 ease-in-out ${
+						isOpen ? 'opacity-0' : ''
+					}`}
+				></div>
+				<div
+					className={`w-8 h-1 bg-orange-600 transform transition-transform duration-300 ease-in-out ${
+						isOpen ? 'origin-center -translate-y-1 -rotate-45' : 'mt-1'
+					}`}
+				></div>
 			</div>
 		</div>
 	);

5c53fe1e0d61b2dd6e78a0e853059ec497612411
Stefan Ihle <stefanihle@gmx.net>
2023-08-25 10:39:07 +0200

    add defineType, defineField and defineArrayMember function to schema

--- sanity/userSchema.ts
+++ sanity/userSchema.ts
@@ -1,15 +1,17 @@
-const userSchema = {
+import { defineType, defineField } from 'sanity';
+
+const userSchema = defineType({
 	name: 'user',
 	title: 'User',
 	type: 'document',
 	fields: [
-		{ name: 'name', type: 'string' },
-		{ name: 'email', type: 'string' },
-		{ name: 'avatar', type: 'image' },
+		defineField({ name: 'name', type: 'string' }),
+		defineField({ name: 'email', type: 'string' }),
+		defineField({ name: 'avatar', type: 'image' }),
 	],
 	preview: {
 		select: { title: 'name', subtitle: 'email', media: 'avatar' },
 	},
-};
+});
 
 export default userSchema;
